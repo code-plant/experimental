02c4ed149477eb7574b8079808ce1d39e4669861
Juyeong Maing <mjy9088@naver.com>
2025-04-27 22:21:36 +0900

    add parallel init script

--- scripts/for-each-package.sh
+++ scripts/for-each-package.sh
@@ -10,15 +10,7 @@ per_package() {
   (cd "$1" && echo ">> Running in $1" && sh -c "$cmd")
 }
 
-for dir1 in "$start_dir"/*/; do
-  [ -d "$dir1" ] || continue
-  for dir2 in "$dir1"*/; do
-    [ -d "$dir2" ] || continue
-    for dir3 in "$dir2"*/; do
-      [ -d "$dir3" ] || continue
-      if [ -f "$dir3/package.json" ]; then
-        per_package "$dir3"
-      fi
-    done
-  done
-done
+while IFS= read -r package; do
+  package=$(echo "$package" | tr -d '\r')
+  per_package "$package"
+done < packages.txt
--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,7 @@ set -e
 cd "$(dirname "$0")/.."
 
 while IFS= read -r package; do
+  package=$(echo "$package" | tr -d '\r')
   echo "Initializing $package"
   (cd "$package" && npm i)
   (cd "$package" && npm run build) || echo "Failed to build $package"
